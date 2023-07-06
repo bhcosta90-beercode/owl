2f067ac7cd13e0a7ec76624749e3cda400435e35
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-06 17:50:09 -0300

    create a structure for database

---
+++ database/migrations/2023_07_06_204548_create_sales_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->on('users');
+            $table->foreignId('customer_id')->on('customers');
+            $table->timestamp('sold_at');
+            $table->char('status', 2);
+            $table->unsignedDouble('total_amount');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
